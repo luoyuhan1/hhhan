1cd415edc54fafa0bc856a490bc8c5fb6b59265f
luoyuhan1 <345744756@qq.com>
2020-06-18 23:39:41 +0800

    第一次上传

---
+++ vuepress_blog_template/docs/.vuepress/config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  "title": "小罗小罗爱吃菠萝",
+  "description": "快乐它又回来了",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitLab",
+            "link": "https://gitlab.com/bobyuan/",
+            "icon": "reco-gitlab"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "新浪微博",
+        "desc": "关注新浪微博，随时随地发生新鲜事",
+        "link": "https://weibo.com/?sudaref=www.baidu.com&display=0&retcode=6102"
+      },
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      }
+    ],
+	"logo": "/logo_trans.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "Luoyuhhhan",
+    "authorAvatar": "/avatar.png",
+    //"record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": false
+  }
+}
